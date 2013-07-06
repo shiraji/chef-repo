bc37975545b79d52a412123275ad18b28d19063d
shiraji <isogai.shiraji@gmail.com>
2013-07-06 15:25:01 +0900

    fix the issues

--- site-cookbooks/addusers/attributes/default.rb
+++ site-cookbooks/addusers/attributes/default.rb
@@ -1,8 +1,8 @@
-default[:adduser][:group][:name] = "shiraji"
+default[:adduser][:group][:name] = "user"
 default[:adduser][:group][:gid] = 10000
-default[:adduser][:name] = "shiraji"
+default[:adduser][:name] = "user"
 default[:adduser][:comment] = "comment"
 default[:adduser][:uid] = 10000
 default[:adduser][:shell] = "/bin/bash"
-default[:adduser][:ssh_rsa] = ["ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAgGB/bZrITA6T1+fwFZApsQnK6CyGJZQsyNBoJVrcbunz7he6zHViOeqAwlLWuSf9bObScvdWD+mqzf18HmXsCMI/9kpyHLs6vPylSOeCeJY/Bn/gK8NRCxsCeniA8ZSkJ57j+hmq9j/Ci6sa+a1ytGpjAj+TwijWsy9KE1vuaQk=", "TEST2"]
-default[:adduser][:addToWheel] = true
+default[:adduser][:ssh_rsa] = [""]
+default[:adduser][:addToWheel] = false
