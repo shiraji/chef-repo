934bbeb53fd728698b0440f5d02087bce0fd4bc9
shiraji <isogai.shiraji@gmail.com>
2013-07-06 12:24:27 +0900

    やっぱり複数ユーザ対応はここでやるべきじゃないと思ったので、戻し。単数対応のみなので、user1を削除

--- site-cookbooks/addusers/attributes/default.rb
+++ site-cookbooks/addusers/attributes/default.rb
@@ -1,11 +1,8 @@
-default['adduser']['user1']['group']['name'] = "shiraji"
-default['adduser']['user1']['group']['gid'] = 10000
-default['adduser']['user1']['name'] = "shiraji"
-default['adduser']['user1']['comment'] = "comment"
-default['adduser']['user1']['uid'] = 10000
-default['adduser']['user1']['shell'] = "/bin/bash"
-#ここが微妙。ssh鍵ごとに設定したいなー
-default['adduser']['user1']['ssh_rsa']['A'] = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAgGB/bZrITA6T1+fwFZApsQnK6CyGJZQsyNBoJVrcbunz7he6zHViOeqAwlLWuSf9bObScvdWD+mqzf18HmXsCMI/9kpyHLs6vPylSOeCeJY/Bn/gK8NRCxsCeniA8ZSkJ57j+hmq9j/Ci6sa+a1ytGpjAj+TwijWsy9KE1vuaQk="
-default['adduser']['user1']['ssh_rsa']['B'] = "TEST"
-default['adduser']['user1']['addToWheel'] = true
-
+default[:adduser][:group][:name] = "shiraji"
+default[:adduser][:group][:gid] = 10000
+default[:adduser][:name] = "shiraji"
+default[:adduser][:comment] = "comment"
+default[:adduser][:uid] = 10000
+default[:adduser][:shell] = "/bin/bash"
+default[:adduser][:ssh_rsa] = ["ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAgGB/bZrITA6T1+fwFZApsQnK6CyGJZQsyNBoJVrcbunz7he6zHViOeqAwlLWuSf9bObScvdWD+mqzf18HmXsCMI/9kpyHLs6vPylSOeCeJY/Bn/gK8NRCxsCeniA8ZSkJ57j+hmq9j/Ci6sa+a1ytGpjAj+TwijWsy9KE1vuaQk=", "TEST2"]
+default[:adduser][:addToWheel] = true
--- site-cookbooks/addusers/recipes/default.rb
+++ site-cookbooks/addusers/recipes/default.rb
@@ -7,57 +7,56 @@
 # All rights reserved - Do Not Redistribute
 #
 
-#それぞれのユーザを設定する。
-node[:adduser].each { |username,user|
-	group user[:group][:name] do
+#ユーザを設定する。
+group node[:adduser][:group][:name] do
+	append true
+	gid node[:adduser][:group][:gid]
+	:create
+end
+
+user node[:adduser][:name] do
+	comment node[:adduser][:comment]
+	uid node[:adduser][:uid]
+	gid node[:adduser][:name]
+	home "/home/" + node[:adduser][:name]
+	shell node[:adduser][:shell]
+	username node[:adduser][:name]
+end
+
+if node[:adduser][:addToWheel]
+	group "wheel" do
 		append true
-		gid user[:group][:gid]
-		:create
+		members node[:adduser][:name]
+		system true
+		:modify
 	end
+end
 
-	user user[:name] do
-		comment user[:comment]
-		uid user[:uid]
-		gid user[:name]
-		home "/home/" + user[:name]
-		shell user[:shell]
-		username user[:name]
-	end
+directory "/home/" + node[:adduser][:name] + "/.ssh/" do
+	owner node[:adduser][:name]
+	group node[:adduser][:group][:name]
+	mode "00755"
+	action :create
+end
 
-	if user[:addToWheel]
-		group "wheel" do
-			append true
-			members user[:name]
-			system true
-			:modify
-		end
-	end
+# authorized_keysの中身を作成する
+begin
+	c = File.read("/home/" + node[:adduser][:name] + "/.ssh/authorized_keys")
+	c += "\n"
+rescue
+	c = ""
+end
 
-	directory "/home/" + user[:name] + "/.ssh/" do
-		owner user[:name]
-		group user[:group][:name]
-		mode "00755"
-		action :create
-	end
+node[:adduser][:user1][:ssh_rsa].each do |ssh_key|
+	c += ssh_key
+	c += "\n"
+end
 
-	# authorized_keysの中身を作成する
-	begin
-		c = File.read("/home/" + user[:name] + "/.ssh/authorized_keys")
-		c += "\n"
-	rescue
-		c = ""
-	end
-
-	node[:adduser][:user1][:ssh_rsa].each do |ssh_key|
-		c += ssh_key[1]
-		c += "\n"
-	end
-
-	file "/home/" + user[:name] + "/.ssh/authorized_keys" do
-			owner user[:name]
-			group user[:group][:name]
-			mode "0600"
-			content c
-			action :create_if_missing
-	end
+file "/home/" + node[:adduser][:name] + "/.ssh/authorized_keys" do
+		owner node[:adduser][:name]
+		group node[:adduser][:group][:name]
+		mode "0600"
+		content c
+		action :create_if_missing
+end
 }
